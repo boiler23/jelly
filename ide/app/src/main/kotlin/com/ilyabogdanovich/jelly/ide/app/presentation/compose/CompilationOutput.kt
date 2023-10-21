7f336f6cf3a002a4fe2fb69b2354a77cea9faa49
Ilya Bogdanovich <boiler23@geo-tracker.org>
2023-10-21 23:41:41 +0200

    Fixed issue with compilation output update

--- ide/app/src/main/kotlin/com/ilyabogdanovich/jelly/ide/app/presentation/compose/CompilationOutput.kt
+++ ide/app/src/main/kotlin/com/ilyabogdanovich/jelly/ide/app/presentation/compose/CompilationOutput.kt
@@ -11,13 +11,10 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import com.ilyabogdanovich.jelly.ide.app.presentation.compose.ds.EditTextStyle
 import com.ilyabogdanovich.jelly.ide.app.presentation.compose.ds.TitleText
@@ -50,19 +47,18 @@ fun CompilationOutputTextField(
     modifier: Modifier = Modifier,
     textColor: Color = MaterialTheme.colors.onSurface,
 ) {
-    var textFieldValue by remember { mutableStateOf(TextFieldValue(value)) }
     val vScrollState = rememberTextFieldScrollState(Orientation.Vertical)
     BasicTextField(
         modifier = modifier,
-        value = textFieldValue,
+        value = value,
         textStyle = EditTextStyle(textColor),
         cursorBrush = SolidColor(MaterialTheme.colors.primary),
-        onValueChange = { textFieldValue = it },
+        onValueChange = {},
         readOnly = true,
         scrollState = vScrollState,
     )
 
-    LaunchedEffect(textFieldValue.text) {
+    LaunchedEffect(value) {
         vScrollState.animateScrollBy(vScrollState.maxOffset)
     }
 }
